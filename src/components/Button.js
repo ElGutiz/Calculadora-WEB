5317a538fd3348fd0f8a1daf21b57c63399ca302
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-04-13 22:40:13 -0600

    Buttons demonstration and style

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "../Styles/Button_style.css";
+
+const isOperator = (val) => !isNaN(val) || val === "." || val === "=";
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div
+        role="presentation"
+        className={`button-wrapper ${isOperator(this.props.children) ? null : "operator"}`}
+        onClick={() => this.props.handleClick(this.props.children)}
+        onClick={this.props.addVal}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Button;
--- src/simpleComponent/startApp.js
+++ src/simpleComponent/startApp.js
@@ -1,15 +1,44 @@
 import React from "react";
 
+import Button from "../components/Button";
+
+import "../Styles/startName_style.css";
+
 class StartApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = props;
+    this.props = props;
   }
 
   render() {
     return (
       <div className="startName">
-        h
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>x</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
+        </div>
       </div>
     );
   }
