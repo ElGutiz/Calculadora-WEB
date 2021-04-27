5b6982ad7a4af66e8aff4a345c0a8500a4f63157
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-04-27 13:07:41 -0600

    Style files implementation fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import 'font-awesome/css/font-awesome.css';
+import "font-awesome/css/font-awesome.css";
 
-import './index.css';
-import StartApp from './simpleComponent/startApp';
+import "./index.css";
+import "./Styles/Button_style.css";
+import "./Styles/clearbutton_style.css";
+import "./Styles/input_style.css";
+import "./Styles/startName_style.css";
+import StartApp from "./simpleComponent/startApp";
 
 ReactDOM.render(
   <StartApp />,
-  document.getElementById('root'),
+  document.getElementById("root"),
 );
